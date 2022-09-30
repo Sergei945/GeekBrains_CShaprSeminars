b7e5f7fee8a5f9224f62a97d99ec4c0b1174de4a
Sergey <fedor945@yandex.ru>
2022-09-30 23:18:40 +0300

    fix ex 004 first seminar

--- homeWorks/FirstSeminar/Program.cs
+++ homeWorks/FirstSeminar/Program.cs
@@ -48,11 +48,22 @@
 // Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 
-// Console.Write("Введите число: ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+if (num1 >= 0) {
+  while (current < num1){
+  if(current % 2 == 0) {
+    Console.Write($" {current}");
+  }
+  current++;
+ }
+} else {
+  while (current > num1){
+  if(current % 2 == 0) {
+    Console.Write($" {current}");
+  }
+  current--;
+ }
+}
 
-// for (int i = 1; i < num1; i++){
-//   if(i % 2 == 0) {
-//     Console.Write($" {i}");
-//   }
-//  }
