5c0d292d034b2af8843a25e9f3aef86f182edf09
Sergey <fedor945@yandex.ru>
2022-09-30 23:02:07 +0300

    Home work Seminar one

--- homeWorks/FirstSeminar/Program.cs
+++ homeWorks/FirstSeminar/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2) {
+//   Console.WriteLine($"{num1} больше чем {num2}");
+// } else {
+//   Console.WriteLine($"{num2} больше чем {num1}");
+// }
+
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2 && num1 > num3) {
+//   Console.WriteLine($"Максимальное из этих чисел первое: {num1}");
+// } else if (num2 > num1 && num2 > num3) {
+//   Console.WriteLine($"Максимальное из этих чисел второе: {num2}");
+// } else {
+//   Console.WriteLine($"Максимальное из этих чисел третье: {num3}");
+// }
+
+
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 % 2 == 0) {
+//   Console.WriteLine($"Да {num1} чётное число");
+// } else {
+//   Console.WriteLine($"Нет {num1} нечётное число");
+// }
+
+// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+// Console.Write("Введите число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i < num1; i++){
+//   if(i % 2 == 0) {
+//     Console.Write($" {i}");
+//   }
+//  }
