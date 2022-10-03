12844d43337fe81c5dc392b2346f6aef2c03b63b
sergey <fedor945@yandex.ru>
2022-10-03 19:08:43 +0300

    fixing my home work

--- homeWorks/FirstSeminar/Program.cs
+++ homeWorks/FirstSeminar/Program.cs
@@ -1,69 +1,69 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// Console.Write("Введите первое число: ");
+// Console.Write("Input first number: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите второе число: ");
+// Console.Write("Input second number: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
 
 // if(num1 > num2) {
-//   Console.WriteLine($"{num1} больше чем {num2}");
+//   Console.WriteLine($"max= {num1}");
 // } else {
-//   Console.WriteLine($"{num2} больше чем {num1}");
+//   Console.WriteLine($"max = {num2}");
 // }
 
 
 // Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-// Console.Write("Введите первое число: ");
+// Console.Write("Input first number ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите второе число: ");
+// Console.Write("Input second number: ");
 // int num2 = Convert.ToInt32(Console.ReadLine());
 
-// Console.Write("Введите третье число: ");
+// Console.Write("Input third number: ");
 // int num3 = Convert.ToInt32(Console.ReadLine());
 
 // if (num1 > num2 && num1 > num3) {
-//   Console.WriteLine($"Максимальное из этих чисел первое: {num1}");
+//   Console.WriteLine($"Max number first: {num1}");
 // } else if (num2 > num1 && num2 > num3) {
-//   Console.WriteLine($"Максимальное из этих чисел второе: {num2}");
+//   Console.WriteLine($"Max number second: {num2}");
 // } else {
-//   Console.WriteLine($"Максимальное из этих чисел третье: {num3}");
+//   Console.WriteLine($"Max number third: {num3}");
 // }
 
 
 
 // Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-// Console.Write("Введите число: ");
+// Console.Write("Input number: ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 
 // if(num1 % 2 == 0) {
-//   Console.WriteLine($"Да {num1} чётное число");
+//   Console.WriteLine($"Yes is {num1} even number");
 // } else {
-//   Console.WriteLine($"Нет {num1} нечётное число");
+//   Console.WriteLine($"No is {num1} uneven number");
 // }
 
 // Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 
-Console.Write("Введите число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int current = 1;
-if (num1 >= 0) {
-  while (current < num1){
-  if(current % 2 == 0) {
-    Console.Write($" {current}");
-  }
-  current++;
- }
-} else {
-  while (current > num1){
-  if(current % 2 == 0) {
-    Console.Write($" {current}");
-  }
-  current--;
- }
-}
+// Console.Write("Input number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+// if (num1 >= 0) {
+//   while (current <= num1){
+//   if(current % 2 == 0) {
+//     Console.Write($" {current}");
+//   }
+//   current++;
+//  }
+// } else {
+//   while (current >= num1){
+//   if(current % 2 == 0) {
+//     Console.Write($" {current}");
+//   }
+//   current--;
+//  }
+// }
 
