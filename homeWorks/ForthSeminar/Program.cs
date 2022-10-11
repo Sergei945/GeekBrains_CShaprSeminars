e90f70bb4bbab4963dda70eb48685899a2708120
Sergey <fedor945@yandex.ru>
2022-10-11 20:42:24 +0300

    forth seminar one

---
+++ homeWorks/ForthSeminar/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// int GetSum(int num) {
+//   int result = 0;
+//   for (int i = 1; i <= num; i++) result += i;
+//   return result;
+// }
+
+// Console.Write("Input a positive integer number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write( $"Sum of numbers from 1 to {a} is {GetSum(a)}");
+
+
+/* */
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int AmountNum(int num) 
+{
+  int corrent = 0;
+  while(num > 0) 
+  {
+    num /= 10;
+    corrent++;
+  }
+  // return num.ToString().Length;
+  return corrent;
+} 
+
+Console.Write("Напиши число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество цифр в числе {num} - {AmountNum(num)}");
+
+
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+
+int Factorial(int num) {
+  int result = 1;
+  for (int i = 1; i <= num; i++) result *= i;
+  return result;
+}
+
+Console.Write("Input a positive integer number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write( $"Factorial of numbers from 1 to {a} is {Factorial(a)}");
+
--- homeWorks/SecondSeminar/Program.cs
+++ homeWorks/SecondSeminar/Program.cs
@@ -45,28 +45,51 @@
 // обозначающую день недели, и проверяет, является ли этот день выходным.
 
 
-bool Weekend(int num)
+// bool Weekend(int num)
+// {
+//     if (num < 8 || num > 0)
+//     {
+//         if (num % 6 == 0 || num % 7 == 0)
+//         {
+//             Console.WriteLine(num);
+//             return true;
+//         }
+//     }
+//     return false;
+
+// }
+
+// // int randomNum =  new Random().Next(1, 7);
+// Console.Write("Введите день недели: ");
+// bool day = Weekend(Convert.ToInt32(Console.ReadLine()));
+// if (day == true)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+
+void WeekendTwoVersion(int num)
 {
+    
     if (num < 8 || num > 0)
     {
         if (num % 6 == 0 || num % 7 == 0)
         {
-            Console.WriteLine(num);
-            return true;
+            Console.WriteLine("Yes");
+
+        } 
+        else 
+        {
+            Console.WriteLine("No");
         }
     }
-    return false;
-
 }
 
-// int randomNum =  new Random().Next(1, 7);
 Console.Write("Введите день недели: ");
-bool day = Weekend(Convert.ToInt32(Console.ReadLine()));
-if (day == true)
-{
-    Console.WriteLine("Yes");
-}
-else
-{
-    Console.WriteLine("No");
-}
+int day = Convert.ToInt32(Console.ReadLine());
+
+WeekendTwoVersion(day);
